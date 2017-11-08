ce734d0e1cab08fb627b3418a0f7925aa490ba90
kakudo415 <31888089+kakudo415@users.noreply.github.com>
2017-11-08 16:05:57 +0900

    dev

---
+++ public/javascripts/files.js
@@ -0,0 +1,21 @@
+'use strict';
+let data = {
+  dir: {
+    dir1: {
+      "id": 2
+    },
+    dir2: {
+      "id": 3
+    },
+    dir3: {
+      "id": 4
+    },
+    "id": 1
+  },
+  memo: {
+    memo1: 1,
+    memo2: 2
+  },
+  "id": 0
+};
+let currentDirectory = 0;
