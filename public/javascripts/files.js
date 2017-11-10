3560d9cd9655e8b3c0c597962969d36a130e4bd8
kakudo415 <31888089+kakudo415@users.noreply.github.com>
2017-11-11 02:42:03 +0900

    dev

--- public/javascripts/files.js
+++ public/javascripts/files.js
@@ -134,6 +134,7 @@ function renameMemo() {
     currentDirectory.memo[newName] = currentDirectory.memo[currentMemo];
     delete currentDirectory.memo[currentMemo];
     showMemo(newName);
+    showDirectory();
   }).fail(() => {
     console.log("Sorry.Communication with the server failed.");
   });
